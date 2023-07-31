68d06d880c8a06c96b0ab838bb2e3976bc5c33f6
Springysoul <132086494+Springysoul@users.noreply.github.com>
2023-08-01 00:18:17 +0300

    Camera Look
    Turn left/right

--- Player.gd
+++ Player.gd
@@ -1,31 +1,37 @@
 extends Node3D
 
 const SENSITIVITY = 0.005
-var ldegrees = 0
-var rdegrees
-var degrees = 0
+var location = 1
 var moving = false
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
 
 
 func _on_detect_left_mouse_entered() -> void:
-	degrees = head.rotation_degrees.y + 90
-	if moving == false:
-		ldegrees = head.rotation_degrees.y
-		moving = true
-		while head.rotation_degrees.y < degrees:
-			head.rotate_y(0.1)
-			await get_tree().create_timer(0.01).timeout
-		moving = false
-
+	#checks if you r looking forward
+	if moving == false and head.rotation_degrees.y == 0:
+		look_left()
 
 func _on_detect_right_mouse_entered() -> void:
-	degrees = head.rotation_degrees.y - 90
-	rdegrees = head.rotation_degrees.y
-	if moving == false and rdegrees > -90:
-		moving = true
-		while head.rotation_degrees.y > degrees:
-			head.rotate_y(-0.1)
-			await get_tree().create_timer(0.01).timeout
-		moving = false
+	if moving == false and head.rotation_degrees.y == 0:
+		look_right()
+
+
+func look_left():
+	#turns your head to the left door
+	moving = true
+	while head.rotation_degrees.y < 90:
+		head.rotate_y(0.1)
+		await get_tree().create_timer(0.01).timeout
+	head.rotation_degrees.y = 90
+	moving = false
+
+
+func look_right():
+	#turns your head to the right door
+	moving = true
+	while head.rotation_degrees.y < -90:
+		head.rotate_y(-0.1)
+		await get_tree().create_timer(0.01).timeout
+	head.rotation_degrees.y = -90
+	moving = false
